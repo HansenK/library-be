c00739439ba87da187b9bfe7a564055f3a36ad77
Thiago Klein <tklein@thirtycapital.com>
2021-10-10 11:52:11 -0300

    Add search field to books page admin

--- books/admin.py
+++ books/admin.py
@@ -4,6 +4,7 @@ from books.models import Book, Tag
 
 class BookAdmin(admin.ModelAdmin):
   list_display = ('title', 'get_authors')
+  search_fields = ('title',)
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
